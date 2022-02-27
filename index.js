fa242b758579dd705fa8f8220339c5b1595c58ca
Abbush <glgberat@yahoo.com>
2022-02-27 05:52:50 -0500

    Templates

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-//const path = require("path");
-//const fs = require("fs");
-
-//const OUTPUT_DIR = path.resolve(__dirname, "output");
-//const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-//const render = require("./lib/htmlRenderer");
 
 
 const team = [];
@@ -134,9 +127,9 @@ function intern() {
   });
 }
 
-/*function generateTeam() {
+function writeTeam() {
   fs.writeFileSync(outputPath, render(members), "utf-8");
-} */
+} 
 
 function generateTeam() {
   inquirer
@@ -155,7 +148,7 @@ function generateTeam() {
         engineer();
       } else {
         console.log(team);
-        //generateTeam();
+        writeTeam();
       }
     });
 }
